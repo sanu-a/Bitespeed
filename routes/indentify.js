e5aa98bec9d3cec8487fdebaabed76be8dc865a0
sanu-a <sanuaenathu@gmail.com>
2024-06-25 01:38:48 +0530

    added comment and bug fixes

--- routes/indentify.js
+++ routes/indentify.js
@@ -14,9 +14,13 @@ route.post("/identify", async (req, res) => {
     return res.status(200).json(result);
   } catch (err) {
     if (err instanceof MyError) {
-      return res.status(400).json({ message: `${err.message}` });
+      return res
+        .status(400)
+        .json({ error: true, success: false, message: `${err.message}` });
     }
-    return res.status(400).json({ message: "failed" });
+    return res
+      .status(400)
+      .json({ error: true, success: false, message: "Internal server error!" });
   }
 });
 
